e185e75d0eba9cdef128ce87d66ad901248728d7
sbr18334 <balchandar.reddy.sangam@philips.com>
2017-09-06 15:35:53 +0530

    minor fixes

--- src/main/webapp/scripts/login/login.js
+++ src/main/webapp/scripts/login/login.js
@@ -2,33 +2,36 @@
 angular.module('app',[])
   .controller('LoginController', function ($scope) {
 	  
-	$('#error_msg').hide();
-	
-    $scope.error = function(){
-		 $('#error_msg').hide();	  	
-    }
-    
-	$(function(){  //to trigger login on enter
-		 $('#password_field').keypress(function (e) {
-		 var key = e.which;
-		 if(key == 13)
-		  {
-		    $scope.homePage();
+	  if(localStorage.user !=null){
+		  $(".bg-wrapper").hide();
+		  window.location.href="/#/overview"
+	  }
+	  
+	  $('#error_msg').hide();
+		
+	  $scope.error = function(){
+		  $('#error_msg').hide();
 		  }
-		})
-	})   
-
-	$scope.homePage=function(){
-	   if(document.getElementById('login_field').value=="admin" && document.getElementById('password_field').value=="admin"
+	    
+	  $(function(){
+		  $('#password_field').keypress(function (e) {
+			  var key = e.which;
+			  if(key == 13){
+				  $scope.homePage();
+				  }
+			  })
+		  })
+		  
+	  $scope.homePage=function(){
+		  if(document.getElementById('login_field').value=="admin" && document.getElementById('password_field').value=="admin"
 		   || document.getElementById('login_field').value=="user" && document.getElementById('password_field').value=="user"){
-	 	  	localStorage.user = document.getElementById('login_field').value;
-	 	  	window.location.href="/#/overview"
-	 	  		} 
-	   else{
-		   if($scope.username!=null && $scope.password!=null){
-		   $('#error_msg').show();
-		   }
-	   }
-	}
-	
-});
+			  localStorage.user = document.getElementById('login_field').value;
+	 	  	  window.location.href="/#/overview";
+	 	  	  }
+		  else{
+			  if($scope.username!=null && $scope.password!=null){
+				  $('#error_msg').show();
+				  }
+			  }
+		  }
+	  });
