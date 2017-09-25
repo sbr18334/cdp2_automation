594fa8c08e8eb33f02fa6adb364a068a95370444
sbr18334 <balchandar.reddy.sangam@philips.com>
2017-09-25 15:59:34 +0530

    Forgot Password

--- src/main/webapp/scripts/password/password.js
+++ src/main/webapp/scripts/password/password.js
@@ -1,4 +1,11 @@
 angular.module('app',[])
   .controller('PasswordController', function ($scope) {
-  
+	  
+	  $('#success').hide();
+	  $('#error').hide();
+	  
+	  $scope.homePage=function(){
+		  $('#success').show();
+	  };
+	  
   });
