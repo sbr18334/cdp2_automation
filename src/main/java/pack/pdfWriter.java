6144a30b90d7e3de403a92e27c0608e56bdc9824
sbr18334 <balchandar.reddy.sangam@philips.com>
2017-09-26 15:27:47 +0530

    Reset Password Email

--- src/main/java/pack/pdfWriter.java
+++ src/main/java/pack/pdfWriter.java
@@ -1,7 +1,13 @@
 package pack;
 
-import com.pdfcrowd.*;
-import java.io.*;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,47 +17,57 @@ import javax.servlet.http.HttpServletResponse;
 public class pdfWriter extends HttpServlet{
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try 
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+		String to = req.getParameter("email").toString();
+		String config = req.getParameter("config").toString();
+		
+        final String username = "sbr18334@gmail.com";
+        final String password = "9949120639";
+
+        Properties props = new Properties();
+        //
+        props.put("mail.smtp.user",username); 
+        props.put("mail.smtp.host", "smtp.gmail.com"); 
+        props.put("mail.smtp.port", "25"); 
+        props.put("mail.debug", "true"); 
+        props.put("mail.smtp.auth", "true"); 
+        props.put("mail.smtp.starttls.enable","true"); 
+        props.put("mail.smtp.EnableSSL.enable","true");
+
+        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");   
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");   
+        props.setProperty("mail.smtp.port", "587");   
+        props.setProperty("mail.smtp.socketFactory.port", "465");
+        //
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("donot-reply@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(to));
+            message.setSubject("Reset-Password link");
+            message.setText("Dear User,"
+                + "\n\n Click on the following link to reset the password\n\n"
+            	+ config+"/scripts/reset/reset.html");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } 
+
+        catch (MessagingException e) 
         {
-            FileOutputStream fileStream;     
- 
-            // create an API client instance
-            Client client = new Client("spcr92", "42e987f6032006611081a622469b0b43");
-
-            // convert a web page and save the PDF to a file
-            fileStream = new FileOutputStream("example.pdf");
-            //client.convertURI("http://cdp2uiatomation-env.wvwrjpmde3.us-east-2.elasticbeanstalk.com/scripts/login/login.html", fileStream);
-            //client.convertURI("http://pdfcrowd.com/forums/read.php?3,666", fileStream);
-            client.convertURI("http://pdfcrowd.com/forums/read.php?3,666",fileStream);
-            //client.convertURI("http://www.cdp2uiatomation-env.wvwrjpmde3.us-east-2.elasticbeanstalk.com/#/error", fileStream);
-            //client.convertURI("http://www.cdp2uiatomation-env.wvwrjpmde3.us-east-2.elasticbeanstalk.com/#/overview/performance", fileStream);
-            fileStream.close();
-            fileStream = new FileOutputStream("example.pdf");
-            client.convertURI("http://google.com",fileStream);
-            fileStream.close();
-            System.out.println("done");
-/*
-            // convert an HTML string and store the PDF into a byte array
-            ByteArrayOutputStream memStream  = new ByteArrayOutputStream();
-            String html = "<html><body>In-memory HTML.</body></html>";
-            client.convertHtml(html, memStream);
-
-            // convert an HTML file
-//            fileStream = new FileOutputStream("file.pdf");
-//            client.convertFile("C:/Users/310231680/Downloads/file.html", fileStream);
-//            fileStream.close();
-*/
-            // retrieve the number of credits in your account
-            Integer ncredits = client.numTokens();
+            e.printStackTrace();
+            System.out.println("Username or Password are incorrect ... exiting !");
         }
-        catch(PdfcrowdError why) {
-            System.err.println(why.getMessage());
-        }
-        catch(IOException exc) {
-            // handle the exception
-        }
-        System.out.println("done from outside");
     }
 
 }
